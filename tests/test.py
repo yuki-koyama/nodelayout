02c83b8353439d1f68b28e00106942c41c2311c4
Yuki Koyama <yuki@koyama.xyz>
2020-02-11 12:03:07 +0900

    Fix path and add a debug message

--- tests/test.py
+++ tests/test.py
@@ -4,7 +4,10 @@ import bpy
 import sys
 import os
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__) + "../.."))
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "../..")
+
+print(sys.path)
+
 import nodelayout
 
 material = bpy.data.materials["Material"]
