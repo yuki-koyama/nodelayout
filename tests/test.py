e29045c588bfbc75d1cca34c0e2579d8b237b450
Yuki Koyama <yuki@koyama.xyz>
2020-02-11 12:05:05 +0900

    Fix path

--- tests/test.py
+++ tests/test.py
@@ -4,7 +4,7 @@ import bpy
 import sys
 import os
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "../..")
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../..")
 
 print(sys.path)
 
